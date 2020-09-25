87f8e6331e42191931c17d5cad30954a03f5ddba
misi6670 <55701043+misi6670@users.noreply.github.com>
2020-09-25 18:33:44 +0300

    Add files via upload

--- v0.1_masyvas.cpp
+++ v0.1_masyvas.cpp
@@ -18,7 +18,7 @@ using std::setfill;
 using std::fixed;
 using std::setprecision;
 
-struct duomuo {
+struct studentas {
     string Vardas;
     string Pavarde;
     int nd[15];
@@ -29,40 +29,42 @@ struct duomuo {
 
 int main()
 {
-    duomuo Eil; duomuo Eil_masyvas[20];
-    vector <duomuo> Eil_vect;
+    vector <studentas> Eil_vect;
     const char separator = ' ';
     const int VardSimb = 15;
     const int PavSimb = 15;
     const int GalutSimb = 16;
-    cout << "Iveskite mokiniu skaiciu (iki 20)\n";
-    int MokSkai;
-    cin >> MokSkai;
-    for (int i = 0; i < MokSkai; i++) {
-        cout << "\n Iveskite studento varda ir pavarde \n";
-        cin >> Eil_masyvas[i].Vardas >> Eil_masyvas[i].Pavarde;
-        cout << "\n Iveskite studento pazymius (iki 15), suvedus visus pazymius irasykite 0\n";
+    int StudSkai=0;
+    cout << "Iveskite studentu skaiciu\n";
+    cin >> StudSkai;
+    studentas* grupe = new studentas[StudSkai];
+    for (int i = 0; i < StudSkai; i++) {
+        cout << "\n Iveskite " << i+1 << " studento varda ir pavarde \n";
+        cin >> grupe[i].Vardas >> grupe[i].Pavarde;
+        cout << "\n Iveskite " << i + 1 << " studento pazymius, suvedus visus pazymius irasykite 0\n";
         int nulis = 1;
-            while (nulis != 0) {
-                cin >> Eil_masyvas[i].nd[Eil_masyvas[i].n];
-                Eil_masyvas[i].galutinis = Eil_masyvas[i].galutinis + (float)Eil_masyvas[i].nd[Eil_masyvas[i].n];
-                if (Eil_masyvas[i].nd[Eil_masyvas[i].n] == 0) nulis = 0;
-                else Eil_masyvas[i].n++;
+        while (nulis != 0) {
+            cin >> grupe[i].nd[grupe[i].n];
+            grupe[i].galutinis = grupe[i].galutinis + (float)grupe[i].nd[grupe[i].n];
+            if (grupe[i].nd[grupe[i].n] == 0) nulis = 0;
+            else {
+                grupe[i].n++;
+                cout << "Ivedete " << grupe[i].n << " pazymi(-ius)" << endl;
             }
-        cout << "\n Iveskite studento egzamino rezultata \n";
-        cin >> Eil_masyvas[i].egz;
-        Eil_masyvas[i].galutinis = Eil_masyvas[i].galutinis / (float) Eil_masyvas[i].n;
-        Eil_masyvas[i].galutinis = Eil_masyvas[i].galutinis * 0.4 + Eil_masyvas[i].egz * 0.6;
+        }
+        cout << "\n Iveskite " << i + 1 << " studento egzamino rezultata \n";
+        cin >> grupe[i].egz;
+        grupe[i].galutinis = grupe[i].galutinis / (float) grupe[i].n;
+        grupe[i].galutinis = grupe[i].galutinis * 0.4 + grupe[i].egz * 0.6;
     }
-
-    cout << left << setw(VardSimb) << setfill(separator) << "Vardas";
+    cout << left << setw(VardSimb) << setfill(separator) << "\n Vardas";
     cout << left << setw(PavSimb) << setfill(separator) << "Pavarde";
     cout << left << setw(GalutSimb) << setfill(separator) << "Galutinis (Vid.)" << endl;
     cout << string( VardSimb + PavSimb + GalutSimb, '-') << endl;
-    for (int i = 0; i < MokSkai; i++) {
-        cout << left << setw(VardSimb) << setfill(separator) << Eil_masyvas[i].Vardas;
-        cout << left << setw(PavSimb) << setfill(separator) << Eil_masyvas[i].Pavarde;
-        cout << left << setw(GalutSimb) << setfill(separator) << fixed << setprecision(2) << Eil_masyvas[i].galutinis << endl;
+    for (int i = 0; i < StudSkai; i++) {
+        cout << left << setw(VardSimb) << setfill(separator) << grupe[i].Vardas;
+        cout << left << setw(PavSimb) << setfill(separator) << grupe[i].Pavarde;
+        cout << left << setw(GalutSimb) << setfill(separator) << fixed << setprecision(2) << grupe[i].galutinis << endl;
     }
     return(0);
 }
